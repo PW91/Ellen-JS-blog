feb7ac2abc78b5eefd4d3f4b265c434a1f1416c8
Przemyslaw Wieteska <wieteska.przemyslaw@gmail.com>
2019-01-04 00:14:40 +0100

    up

--- models/Gallery.js
+++ models/Gallery.js
@@ -1,4 +1,4 @@
-var keystone = require('keystone');
+var keystone = require("keystone");
 var Types = keystone.Field.Types;
 
 /**
@@ -6,15 +6,15 @@ var Types = keystone.Field.Types;
  * =============
  */
 
-var Gallery = new keystone.List('Gallery', {
-	autokey: { from: 'name', path: 'key', unique: true },
+var Gallery = new keystone.List("Gallery", {
+	autokey: { from: "name", path: "key", unique: true }
 });
 
 Gallery.add({
 	name: { type: String, required: true },
-	publishedDate: { type: Date, default: Date.now },
-	heroImage: { type: Types.CloudinaryImage },
-	images: { type: Types.CloudinaryImages },
+	publishedDate: { type: Date, default: Date.now }
+	//heroImage: { type: Types.CloudinaryImage },
+	//images: { type: Types.CloudinaryImages },
 });
 
 Gallery.register();
